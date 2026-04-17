36ac71a87e7b039f87b79557a6b6d18dc10cb503
infrizzi <luca.paladino26@gmail.com>
2026-04-17 11:51:22 +0200

    Uso GPU più potenti per il testing

--- testing_standard.sh
+++ testing_standard.sh
@@ -3,12 +3,12 @@
 #SBATCH --output=logs/test_standard/test_%j.out
 #SBATCH --error=logs/test_standard/test_%j.err
 #SBATCH --account=tesi_lpaladino
-#SBATCH --partition=all_usr_prod
+#SBATCH --partition=boost_usr_prod
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=40G
 #SBATCH --time=1:00:00
-#SBATCH --constraint=gpu_A40_45G|gpu_L40S_45G|gpu_RTX6000_24G|gpu_RTX_A5000_24G
+#SBATCH --constraint=gpu_A40_45G|gpu_L40S_45G
 
 # Modules
 module unload python/3.11.11-gcc-11.4.0
